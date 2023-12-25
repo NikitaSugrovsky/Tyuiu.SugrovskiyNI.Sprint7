d0d9877f6f875ab674ca620806d0cb8d2f899997
Nikita Sugrovsky <sugrovskiy01@bk.ru>
2023-12-25 20:06:39 +0500

    Sprint7

--- Tyuiu.SugrovskiyNI.Sprint7.Project.V6.Test/DataServiceTest.cs
+++ Tyuiu.SugrovskiyNI.Sprint7.Project.V6.Test/DataServiceTest.cs
@@ -13,7 +13,51 @@ namespace Tyuiu.SugrovskiyNI.Sprint7.Project.V6.Test
         {
             DataService ds = new DataService();
 
-            string path = @"C:\Users\Admin\source\repos\Tyuiu.SugrovskiyNI.Sprint7\Tyuiu.SugrovskiyNI.Sprint7.Project.V6\bin\Debug\OutputFile.csv";
+            string path = @"C:\Users\Admin\source\repos\Tyuiu.SugrovskiyNI.Sprint7\Tyuiu.SugrovskiyNI.Sprint7.Project.V6\bin\Debug\OutputFile_1.csv";
+            FileInfo fileinfo = new FileInfo(path);
+            bool fileex = fileinfo.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, fileex);
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            DataService ds = new DataService();
+
+            string path = @"C:\Users\Admin\source\repos\Tyuiu.SugrovskiyNI.Sprint7\Tyuiu.SugrovskiyNI.Sprint7.Project.V6\bin\Debug\OutputFile_2.csv";
+            FileInfo fileinfo = new FileInfo(path);
+            bool fileex = fileinfo.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, fileex);
+        }
+        [TestMethod]
+        public void TestMethod3()
+        {
+            DataService ds = new DataService();
+
+            string path = @"C:\Users\Admin\source\repos\Tyuiu.SugrovskiyNI.Sprint7\Tyuiu.SugrovskiyNI.Sprint7.Project.V6\bin\Debug\OutputFile_3.csv";
+            FileInfo fileinfo = new FileInfo(path);
+            bool fileex = fileinfo.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, fileex);
+        }
+        [TestMethod]
+        public void TestMethod4()
+        {
+            DataService ds = new DataService();
+
+            string path = @"C:\Users\Admin\source\repos\Tyuiu.SugrovskiyNI.Sprint7\Tyuiu.SugrovskiyNI.Sprint7.Project.V6\bin\Debug\OutputFile_4.csv";
+            FileInfo fileinfo = new FileInfo(path);
+            bool fileex = fileinfo.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, fileex);
+        }
+        [TestMethod]
+        public void TestMethod5()
+        {
+            DataService ds = new DataService();
+
+            string path = @"C:\Users\Admin\source\repos\Tyuiu.SugrovskiyNI.Sprint7\Tyuiu.SugrovskiyNI.Sprint7.Project.V6\bin\Debug\OutputFile_5.csv";
             FileInfo fileinfo = new FileInfo(path);
             bool fileex = fileinfo.Exists;
             bool wait = true;
